6761d10e4f2813618106df69b6ae455aab6542bd
InfinityOverflow <ayushforwork.in@gmail.com>
2024-04-03 04:27:09 +0530

    Added-checkout-orderStatus-stripe

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,21 +4,26 @@ import "dotenv/config";
 import mongoose from 'mongoose';
 import myUserRoute  from "./routes/MyUserRoute";
 import restaurantRoute from "./routes/RestaurantRoutes"
+import orderRoute from "./routes/OrderRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>{
     console.log('connected to DB!!!');
 });
 
 const app =express();
-app.use(express.json());
+
 app.use(cors())
 
 app.get("/test" , async (req: Request,res: Response)=>{
     res.json({message: "Hello !!"});
 });
 
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
 app.use("/api/my/user",myUserRoute);
 app.use("/api/restaurant",restaurantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(7000,()=>{
     console.log("server started");
--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -5,6 +5,8 @@ import AuthCallbackPage from "./pages/AuthCallbackPage"
 import UserProfilePage from "./pages/UserProfilePage"
 import ProtectedRoute from "./auth/ProtectedRoute"
 import SearchPage from "./pages/SearchPage"
+import DetailPage from "./pages/DetailPage"
+import OrderStatusPage from "./pages/OrderStatusPage"
 
 export const AppRoutes=()=>{
     return(
@@ -12,6 +14,13 @@ export const AppRoutes=()=>{
             <Route path="/" element ={<Layout showHero={true}><HomePage/></Layout>}/>
             <Route path="/auth-callback" element ={<AuthCallbackPage/>}/>
             <Route element={<ProtectedRoute/>}>
+            <Route
+          path="/order-status" element={
+            <Layout>
+              <OrderStatusPage />
+            </Layout>
+          }
+        />    
             <Route path="/user-profile" element ={<Layout>
                 <UserProfilePage/>
             </Layout>}/>
@@ -19,6 +28,7 @@ export const AppRoutes=()=>{
             <Route path="/search/:city" element ={<Layout showHero={false}>
             <SearchPage/>
             </Layout>}/>
+            <Route path="/detail/:restaurantId" element={<Layout showHero={false}> <DetailPage /> </Layout>}/>
             <Route path="*" element ={<Navigate to="/"/>}/>
         </Routes>
     )
