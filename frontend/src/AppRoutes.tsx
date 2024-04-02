c4c305c6a75be96e4101828b35357ab6c0ad50aa
InfinityOverflow <ayushforwork.in@gmail.com>
2024-04-02 14:04:33 +0530

    auth-and-backend-changes

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import Layout from "./layout/layout"
 import HomePage from "./pages/HomePages"
+import AuthCallbackPage from "./pages/AuthCallbackPage"
 
 export const AppRoutes=()=>{
     return(
         <Routes>
             <Route path="/" element ={<Layout><HomePage/></Layout>}/>
+            <Route path="/auth-callback" element ={<AuthCallbackPage/>}/>
             <Route path="/user-profile" element ={<span>Profile PAGE</span>}/>
             <Route path="*" element ={<Navigate to="/"/>}/>
         </Routes>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom/client'
 import './global.css'
 import { BrowserRouter as Router } from "react-router-dom";
 import { AppRoutes } from './AppRoutes.tsx';
+import { QueryClient, QueryClientProvider } from "react-query";
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate.tsx';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
+      <QueryClientProvider client={queryClient}>
       <Auth0ProviderWithNavigate>
       <AppRoutes/>
       </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
 )
